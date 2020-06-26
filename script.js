95fa71119e5e77fcdfcae2a5f59efaea3ca23d5f
Jeremy <jeremyVDLRC@gmail.com>
2020-06-26 23:17:01 +1000

    console logging response from weather API

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ function searchWeather() {
     let searchValue = searchInput.val();
     const apiKey = "724e98b55891110350e3c7d68a2fcece";
 
-    const queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + apiKey;
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + searchValue + "&appid=" + apiKey;
 
     console.log(searchValue);
     console.log(queryURL);
 
-
-
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    })
 }
