6deb1f46fe67063d2fffaf5bbb75cbfdc7a320ed
Jeremy <jeremyVDLRC@gmail.com>
2020-06-27 00:05:09 +1000

    current temperature now updates when search is triggered, created variables for current humidity and windSpeed - UV index will need to taken from one call API from open weather Map -> almost all information (except city name) available from other API but must search using coordinates

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const searchInput = $('#searchCity');
 const searchBtn = $('#submitBtn');
+const currentCity = $("#currentCity");
+const currentTemp = $("#currentTemp");
+const currentHumidity = $("#currentHumidity");
+const currentWind = $("#currentWind");
+const currentUV = $("#currentUV");
 
 
 searchBtn.on('click', function(event) {
@@ -26,8 +31,21 @@ function searchWeather() {
         console.log(response.coord.lon);
 
 
-
         const tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        const tempC = (tempF - 32) * (5 / 9);
+
+        console.log(tempF.toFixed(2));
+        console.log(tempC.toFixed(2));
+
+        const humidity = response.main.humidity;
+        const windSpeed = response.wind.speed;
+        const UV = //need to use One Call API from Open Weather Map API
+
+
+            currentTemp.text("Temperature: " + tempC.toFixed(2) + " °C");
+
+
+
 
         let coordinates = "lat=" + response.coord.lat + "&lon=" + response.coord.lon;
         console.log(coordinates);
