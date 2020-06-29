9ce1b92d7cebe619fbd2e9b9851dad25089afdd8
Jeremy <jeremyVDLRC@gmail.com>
2020-06-29 23:34:44 +1000

    cleaned up code, removed console logs, added screenshots and updated README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # WeatherDashboard
 Full Stack Flex Homework Activity #6
+
+# Description:
+
+The project involves creating a Weather Dashboard that allows users to search a city's name and the browser will dynamically display a city's current weather as well as temperature and humidity for the following 5 days. This demonstrates our ability to access the OpenWeather Remote API and return information from the AJAX request to the user on the browser.
+
+
+# Keypoints:
+* Demonstrate requesting information in remote APIs
+* Calling a second Ajax request to a different API using the response from another ajax method request.
+* Dynamically create and clear html elements with each search to display new forecast information and search history
+* Locally store last searched city and automatically load the last searched city weather details upon refresh/load of the browser
+
+
+
+
+1. Upon launch of browser, users are greeted with the following screen prompting them to search a city of their choice
+
+
+![](/screenshots/1.png)
+
+
+2. Once user enters a city name and presses the search button, the browser will dynamically display the following weather-related information for the current date in the aforementioned city:
+ - Temperature
+ - Humidity
+ - Wind Speed
+ - UV Index
+
+
+![](/screenshots/2.png)
+
+
+ 2a. User's search History will also generate a new button beneath the search bar
+ 
+ 2b. 5 boxes will also appear showing the temperature and humidity percentage for the following 5 days with their respective dates
+
+
+![](/screenshots/3.png)
+
+ 3. When User searches a different city, this will append a new button to the search history, which when clicked, will show weather details for that city.
+
+
+![](/screenshots/4.png)
+
+
+ 4. upon refresh of the page, the last searched city's weather details will be displayed
+
+
+![](/screenshots/5.png)
+
+
+
+ # Issues/future features to add:
+ * Unable to implement styling to UV Index which will display a different background colour depending on the UV Index value
+* Unable to implement weather icon, for each day - some code still in source code commented out
+
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchInput = $('#searchCity');
 const searchBtn = $('#submitBtn');
 const currentCity = $("#currentCity");
+const currentIcon = $("currentIcon");
 const currentTemp = $("#currentTemp");
 const currentHumidity = $("#currentHumidity");
 const currentWind = $("#currentWind");
@@ -20,168 +21,166 @@ window.onload = function() {
     } else {
         searchCityCoordinates(JSON.stringify(previousSearch).trim().slice(1, -1))
     }
+}
 
 
-    searchBtn.on('click', function(event) {
-        event.preventDefault();
-        clearForecast();
-        input = searchInput.val().trim();
-        console.log(input);
-        searchCityCoordinates(input);
-    })
+searchBtn.on('click', function(event) {
+    event.preventDefault();
+    clearForecast();
+    input = searchInput.val().trim();
+    console.log(input);
+    searchCityCoordinates(input);
+})
 
-    function searchCityCoordinates(searchValue) {
-
-
-        const apiKey = "724e98b55891110350e3c7d68a2fcece";
-
-        const queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + searchValue + "&appid=" + apiKey;
-
-        console.log(searchValue);
-        console.log(queryURL);
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            console.log(response.coord.lat);
-            console.log(response.coord.lon);
-
-            let UTC = response.dt;
-            let d = new Date(0);
-            d.setUTCSeconds(UTC);
-            console.log(d);
-
-            currentCity.text(response.name + " - " + d.toString().slice(3, 15));
+function searchCityCoordinates(searchValue) {
 
 
-            coordinates = "lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+    const apiKey = "724e98b55891110350e3c7d68a2fcece";
 
-            searchHistory(response.name);
-            searchWeather(coordinates);
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + searchValue + "&appid=" + apiKey;
 
-            localStorage.setItem("previousSearch", JSON.stringify(response.name));
+    console.log(searchValue);
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        let UTC = response.dt;
+        let d = new Date(0);
+        d.setUTCSeconds(UTC);
 
 
-        })
+        currentCity.text(response.name + " - " + d.toString().slice(3, 15));
 
-    }
+        // currentIcon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].id + ".png");
 
-    $(document).on('click', '.history', function(event) {
-        event.preventDefault();
-        console.log($(this).text());
+
+
+        coordinates = "lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+
+        searchHistory(response.name);
+        searchWeather(coordinates);
+
+        localStorage.setItem("previousSearch", JSON.stringify(response.name));
 
 
     })
 
+}
+
+$(document).on('click', '.history', function(event) {
+    event.preventDefault();
+
+    clearForecast();
+    searchCityCoordinates($(this).text());
+
+})
 
 
 
 
-    function searchWeather(x) {
 
-        const apiKey = "724e98b55891110350e3c7d68a2fcece";
-        const queryURL2 = "https:api.openweathermap.org/data/2.5/onecall?" + x + "&appid=" + apiKey;
+function searchWeather(x) {
 
-        $.ajax({
-            url: queryURL2,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
+    const apiKey = "724e98b55891110350e3c7d68a2fcece";
+    const queryURL2 = "https:api.openweathermap.org/data/2.5/onecall?" + x + "&appid=" + apiKey;
 
-
-            currentTemp.text("Temperature: " + tempConvert(response.current.temp).toFixed(2) + " °C");
-            currentHumidity.text("Humidity: " + response.current.humidity + "%");
-            currentWind.text("Wind Speed: " + WindConvert(response.current.wind_speed).toFixed(2) + " KPH");
-            currentUV.text("UV Index: " + response.current.uvi);
-
-            for (i = 0 + 1; i < 6; i++) {
-                let newForecast = $('<div>');
-                newForecast.addClass('daily');
-                newForecast.attr('id', 'daily-' + i);
-
-                let forecastDate = $('<p>');
-                let forecastPic = $('<div>')
-                let forecastTemp = $('<p>');
-                let forecastHumidity = $('<p>');
-                forecastDate.addClass('info');
-                forecastPic.addClass('info');
-                forecastTemp.addClass('info');
-                forecastHumidity.addClass('info');
-
-                let UTCi = response.daily[i].dt;
-                let di = new Date(0);
-                di.setUTCSeconds(UTCi);
-                console.log(di);
-
-                forecastDate.text(di.toString().slice(3, 15));
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
 
 
-                forecastTemp.text('Temp: ' + tempConvert(response.daily[i].temp.day).toFixed(2) + '°C');
-                console.log(tempConvert(response.daily[i].temp.day).toFixed(2) + "°C");
+        currentTemp.text("Temperature: " + tempConvert(response.current.temp).toFixed(2) + " °C");
+        currentHumidity.text("Humidity: " + response.current.humidity + "%");
+        currentWind.text("Wind Speed: " + WindConvert(response.current.wind_speed).toFixed(2) + " KPH");
+        currentUV.text("UV Index: " + response.current.uvi);
+
+        for (i = 0 + 1; i < 6; i++) {
+            let newForecast = $('<div>');
+            newForecast.addClass('daily');
+            newForecast.attr('id', 'daily-' + i);
+
+            let forecastDate = $('<p>');
+            let forecastPic = $('<div>')
+            let forecastTemp = $('<p>');
+            let forecastHumidity = $('<p>');
+            forecastDate.addClass('info');
+            forecastPic.addClass('info');
+            forecastTemp.addClass('info');
+            forecastHumidity.addClass('info');
+
+            let UTCi = response.daily[i].dt;
+            let di = new Date(0);
+            di.setUTCSeconds(UTCi);
 
 
-                forecastHumidity.text('Humidity: ' + response.daily[i].humidity + "%");
-                console.log('Humidity:' + response.daily[i].humidity + "%");
+            forecastDate.text(di.toString().slice(3, 15));
+
+
+            forecastTemp.text('Temp: ' + tempConvert(response.daily[i].temp.day).toFixed(2) + '°C');
 
 
 
-                newForecast.append(forecastDate);
-                newForecast.append(forecastPic);
-                newForecast.append(forecastTemp);
-                newForecast.append(forecastHumidity);
+            forecastHumidity.text('Humidity: ' + response.daily[i].humidity + "%");
 
 
-                forecastDiv.append(newForecast);
-
-            }
-
-        })
-    }
 
 
-    function tempConvert(x) {
-        const tempF = (x - 273.15) * 1.80 + 32;
-        const tempC = (tempF - 32) * (5 / 9);
-
-        return tempC;
-    }
-
-    function WindConvert(y) {
-        const windMPH = y;
-        const windKPH = windMPH * 1.6093427125;
-        return windKPH;
-    }
+            newForecast.append(forecastDate);
+            newForecast.append(forecastPic);
+            newForecast.append(forecastTemp);
+            newForecast.append(forecastHumidity);
 
 
-    function searchHistory(x) {
-        let newHistory = $('<div>');
-        newHistory.addClass('history');
-        newHistory.text(x);
-        searchColumn.append(newHistory);
+            forecastDiv.append(newForecast);
 
-    }
-
-    function clearForecast() {
-        forecastDiv.empty();
-        const header = $('<h2>');
-        header.text('5 Day Forecast');
-        header.attr('id', 'forecastHeader');
-        forecastDiv.append(header);
-
-    }
-
-    function CurrentWeatherIcon(x) {
-        if (x === "clear") {
-            $(this).addClass('clear')
-        } else if (x === "Rain") {
-            $(this).addClass('Rain')
-        } else if (x === "Cloudy") {
-            $(this).addClass('Cloud')
         }
 
-
-
-    }
+    })
 }
+
+
+function tempConvert(x) {
+    const tempF = (x - 273.15) * 1.80 + 32;
+    const tempC = (tempF - 32) * (5 / 9);
+
+    return tempC;
+}
+
+function WindConvert(y) {
+    const windMPH = y;
+    const windKPH = windMPH * 1.6093427125;
+    return windKPH;
+}
+
+
+function searchHistory(x) {
+    let newHistory = $('<div>');
+    newHistory.addClass('history');
+    newHistory.text(x);
+    searchColumn.append(newHistory);
+
+}
+
+function clearForecast() {
+    forecastDiv.empty();
+    const header = $('<h2>');
+    header.text('5 Day Forecast');
+    header.attr('id', 'forecastHeader');
+    forecastDiv.append(header);
+
+}
+
+// function CurrentWeatherIcon(x) {
+//     let iconID = x;
+//     let iconURL = "";
+//     iconURL = "http://openweathermap.org/img/w/" + iconID + ".png";
+
+//     return iconURL;
+
+// }
